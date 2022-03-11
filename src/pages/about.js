30257e7acb06924f6102ef2c146bc2d9e07be9fb
Paulina <paulina.gumienna@platform.sh>
2022-03-11 13:49:42 +0100

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+
+import * as React from 'react';
+
+export default function IndexPage(){
+return (
+    <main>
+        <h1>About this site</h1>
+    </main>
+)
+}
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+//homepage for site, def export
+import * as React from 'react';
+
+export default function IndexPage(){
+return (
+    <main>
+        <h1>Hello fm</h1>
+    </main>
+)
+}
